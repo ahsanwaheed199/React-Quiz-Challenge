1a46d89779459f6c02a66d33bffe7c96461ca50b
Ahsan Awan <60344115+ahsanwaheed199@users.noreply.github.com>
2022-04-01 16:09:15 +0500

    fixing Errors

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -11,6 +11,7 @@ const Quiz = () => {
   const [answer, setAnswer] = useState("0%");
   const [len, setLen] = useState(data.length);
   const [maxScore, setMaxScore] = useState("0%");
+  const [disableOptions, setDisableOption] = useState(false);
 
   const currentData = useMemo(() => {
     const QuestionOne = (currentPage - 1) * DisplayQuestion;
@@ -49,6 +50,7 @@ const Quiz = () => {
   //   return (currentPage / correctAnswer.length) * 100;
   // };
   const onNext = () => {
+    setDisableOption(false);
     setCurrentPage(currentPage + 1);
     let updatedprog = (currentPage / data.length) * 100;
     setProgress(`${updatedprog}%`);
@@ -64,14 +66,15 @@ const Quiz = () => {
     setCorrect("");
   };
 
-  const onPrevious = () => {
-    // let updatedprogress = Number(progress) - 5;
-    // setProgress(`${updatedprogress}%`);
-    setCurrentPage(currentPage - 1);
-  };
+  // const onPrevious = () => {
+  //   // let updatedprogress = Number(progress) - 5;
+  //   // setProgress(`${updatedprogress}%`);
+  //   setCurrentPage(currentPage - 1);
+  // };
 
   const handleOptions = (e) => {
-    if (data.find((corAns) => corAns.correct_answer == e.target.value)) {
+    e.target.value && setDisableOption(true);
+    if (currentData.find((corAns) => corAns.correct_answer == e.target.value)) {
       setCorrect("Correct!");
       correctAnswer.push(e.target.value);
     } else setCorrect("Sorry!");
@@ -90,31 +93,32 @@ const Quiz = () => {
         {currentPage < 20 ? (
           currentData.map((value, index) => {
             return (
-              <>
+              <div key={index}>
                 <S.Heading>Question {currentPage} out of 20</S.Heading>
                 <p>Entertainment: Board Game</p>
                 <S.Question>{decodeURIComponent(value.question)}</S.Question>
                 {value.difficulty == "easy" ? (
-                  <span class="fa fa-star checked"></span>
+                  <span className="fa fa-star checked"></span>
                 ) : value.difficulty == "medium" ? (
                   <>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>{" "}
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star checked"></span>{" "}
                   </>
                 ) : (
                   <>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>{" "}
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star checked"></span>{" "}
                   </>
                 )}
-              </>
+              </div>
             );
           })
         ) : (
           <>
             <h2>Your Result is {result()} %</h2>
-            <h2>Correct Answer: {correctAnswer.length}</h2>{" "}
+            <h2>Correct Answer: {correctAnswer.length}</h2>
+
             <h2>Incorrect Answer: {data.length - correctAnswer.length}</h2>
           </>
         )}
@@ -122,20 +126,24 @@ const Quiz = () => {
         <S.OptionsContainer>
           {currentPage < 20 &&
             allOptions().map((val, ind) => (
-              <S.Button value={val} key={ind} onClick={(e) => handleOptions(e)}>
+              <S.Button
+                value={val}
+                key={ind}
+                onClick={(e) => handleOptions(e)}
+                disabled={disableOptions}
+              >
                 {decodeURIComponent(val)}
               </S.Button>
             ))}
-          {console.log(correctAnswer.length)}
-          {console.log(len)}
-          {console.log(maxScore)}
         </S.OptionsContainer>
 
         {currentPage < 20 && <S.Status correct={correct}> {correct}</S.Status>}
         {currentPage < 20 && (
           <S.Pages>
-            <S.Button onClick={onPrevious}>Previous</S.Button>
-            <S.Button onClick={onNext}>Next</S.Button>
+            {/* <S.Button onClick={onPrevious}>Previous</S.Button> */}
+            <S.Button onClick={disableOptions == true ? onNext : undefined}>
+              Next
+            </S.Button>
           </S.Pages>
         )}
       </S.Content>
